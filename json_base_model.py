312f093a993307250fef2779ce12a00760e4e87a
Gordon-Webster <113298960+Gordon-Webster@users.noreply.github.com>
2024-05-31 15:39:45 +0000

    hw4upload

---
+++ json_base_model.py
@@ -0,0 +1,25 @@
+import json
+from pydantic import BaseModel, Field
+
+class Companies(BaseModel):
+    Name: str = Field(alias='Name_Ins')
+    No: str
+    Address: str
+    Tax_ID:str = Field(alias= 'tax_ID_number')
+    X: str = Field(alias= 'wgs84aX')
+    Y:str = Field(alias= 'wgs84aY')
+    
+class Result(BaseModel):
+    records:list[Companies]
+
+class Food_Processing_Company(BaseModel):
+    result:Result
+
+with open('新北市食品工廠清冊.json') as file:
+    content:str = file.read()
+    fpc:Food_Processing_Company = Food_Processing_Company.model_validate_json(content)
+    
+
+fpcs = fpc.result.records
+for c in fpcs:
+    print(c) 
